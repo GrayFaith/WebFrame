f40e2c84a1dae81d398ece527d50694a88548824
GrayFaith <563292861@qq.com>
2018-10-24 10:20:41 +0800

    使用了彩色的日志记录器,修复了一个转发/的时候出现的长度问题

--- src/main/java/org/kjsy/grayfaith/Common/forWard/Controller/ForWardController.java
+++ src/main/java/org/kjsy/grayfaith/Common/forWard/Controller/ForWardController.java
@@ -13,33 +13,41 @@ public class ForWardController {
 
     @RequestMapping("/forward/**.do")
     public String forward(HttpServletRequest request, HttpSession session) {
-        String method = request.getMethod();
-        StringBuilder logStr = new StringBuilder();
-        if ("POST".equals(method)) {
-            logStr.append("==>POSTRequest");
-        } else if ("GET".equals(method)) {
-            logStr.append("==>GETRequest");
-        } else {
-            logStr.append("==>");
-            logStr.append(method);
-            logStr.append("Request");
-        }
-        logStr.append(",请求基地址:");
-        logStr.append(request.getContextPath());
-        logStr.append(",URI:");
-        logStr.append(request.getRequestURI());
-
         String ctx = request.getContextPath() + "/forward/";
         String uri = request.getRequestURI();
+        String path = null;
+        try {
+            String method = request.getMethod();
+            StringBuilder logStr = new StringBuilder();
+            if ("POST".equals(method)) {
+                logStr.append("==>POSTRequest");
+            } else if ("GET".equals(method)) {
+                logStr.append("==>GETRequest");
+            } else {
+                logStr.append("==>");
+                logStr.append(method);
+                logStr.append("Request");
+            }
+            logStr.append(",请求基地址:");
+            logStr.append(request.getContextPath());
+            logStr.append(",URI:");
+            logStr.append(request.getRequestURI());
 
-        String path = uri.substring(uri.indexOf(ctx) + ctx.length());
-        if (path.lastIndexOf(".") > 0) {
-            path = path.substring(0, path.lastIndexOf("."));
+            if ("/".equals(uri)) {
+                path = "/";
+            } else {
+                path = uri.substring(uri.indexOf(ctx) + ctx.length());
+            }
+            if (path.lastIndexOf(".") > 0) {
+                path = path.substring(0, path.lastIndexOf("."));
+            }
+            logStr.append(",访问路径:");
+            logStr.append(path);
+
+            log.info(logStr.toString());
+        } catch (Exception e) {
+            log.error("转发页面出现异常", e);
         }
-        logStr.append(",访问路径:");
-        logStr.append(path);
-
-        log.info(logStr.toString());
         return path;
     }
 }
